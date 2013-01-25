4489131d3a7c24578aba276d6afab9c66bbb9c65
ShrihariBhaskaramurthi <shriharimurthi@gmail.com>
2013-01-24 19:15:12 -0500

    Changes Shrihari Bhaskar

--- RobotProjectAlpha/src/edu/wpi/first/wpilibj/templates/ShrihariBhaskar.java
+++ RobotProjectAlpha/src/edu/wpi/first/wpilibj/templates/ShrihariBhaskar.java
@@ -3,11 +3,73 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.Accelerometer;
 
 /**
  *
  * @author 05561
  */
 public class ShrihariBhaskar {
-    
+    Accelerometer k = new Accelerometer(1);
+    public double getAcceleration()
+    {
+        return k.getAcceleration();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
