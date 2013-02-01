2ed73bc3485ae700787507af51948adbd62a14f9
yalamanchilirahul <yalamanchili.rahul.fhs@gmail.com>
2013-01-31 19:06:29 -0500

    PhotoSensor

--- RobotProjectAlpha/src/edu/wpi/first/wpilibj/templates/RahulYalamanchili.java
+++ RobotProjectAlpha/src/edu/wpi/first/wpilibj/templates/RahulYalamanchili.java
@@ -1,16 +1,27 @@
 /*
- * Color Sensor
+ * Digital Input
  */
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.Dashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  *
  * created by: Rahul Yalamanchili
  */
 public class RahulYalamanchili {
     
+    DigitalInput PhotoSensor = new DigitalInput(1);
+    
+    public boolean get()
+    {
+        return PhotoSensor.get();
+    }
 
-   
+   public void getDashboard(){
+       SmartDashboard.getBoolean("PhotoSensor Value;", PhotoSensor.get);
+   }
             
 }
